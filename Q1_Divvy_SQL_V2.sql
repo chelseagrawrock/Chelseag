13e49fed88bddee1cd043de0199b25a6059cb198
chelseagrawrock <59317012+chelseagrawrock@users.noreply.github.com>
2024-12-28 10:52:33 -0800

    Update Q1_Divvy_SQL_V2.sql
    SQL code written for the Q1 Divvy Analysis

--- Q1_Divvy_SQL_V2.sql
+++ Q1_Divvy_SQL_V2.sql
@@ -1,9 +1,11 @@
----average ride length
+--average ride duration by day of the week and user type
 
 SELECT usertype AS member_casual,
+       DATENAME(WEEKDAY, CAST(starttime AS DATETIME)) AS day_of_week,
        AVG(DATEDIFF(MINUTE, CAST(starttime AS DATETIME), CAST(stoptime AS DATETIME))) AS avg_ride_duration_minutes
 FROM dbo.['Divvy_Trips_2015-Q1$']
-GROUP BY usertype;
+GROUP BY usertype, DATENAME(WEEKDAY, CAST(starttime AS DATETIME))
+ORDER BY usertype, day_of_week;
 
 ---total number of rides by member type per day of the week
 
@@ -14,30 +16,12 @@ FROM dbo.['Divvy_Trips_2015-Q1$']
 GROUP BY usertype, DATENAME(WEEKDAY, CAST(starttime AS DATETIME))
 ORDER BY usertype, total_rides DESC;
 
---max and min ride duration by member type
-
-SELECT usertype AS member_casual,
-       MAX(DATEDIFF(MINUTE, CAST(starttime AS DATETIME), CAST(stoptime AS DATETIME))) AS max_ride_duration_minutes,
-       MIN(DATEDIFF(MINUTE, CAST(starttime AS DATETIME), CAST(stoptime AS DATETIME))) AS min_ride_duration_minutes
-FROM dbo.['Divvy_Trips_2015-Q1$']
-GROUP BY usertype;
-
 --total ride count by user type
 
 SELECT usertype AS member_casual,
        COUNT(trip_id) AS total_trips
 FROM dbo.['Divvy_Trips_2015-Q1$']
 GROUP BY usertype;
-
---average ride duration by day of the week and user type
-
-SELECT usertype AS member_casual,
-       DATENAME(WEEKDAY, CAST(starttime AS DATETIME)) AS day_of_week,
-       AVG(DATEDIFF(MINUTE, CAST(starttime AS DATETIME), CAST(stoptime AS DATETIME))) AS avg_ride_duration_minutes
-FROM dbo.['Divvy_Trips_2015-Q1$']
-GROUP BY usertype, DATENAME(WEEKDAY, CAST(starttime AS DATETIME))
-ORDER BY usertype, day_of_week;
-
 
 --total ride duration by user type
 
@@ -66,3 +50,29 @@ FROM dbo.['Divvy_Trips_2015-Q1$']
 GROUP BY to_station_name, usertype
 ORDER BY usertype, total_rides DESC
 LIMIT 10;
+
+--Code elow was created when creating analysis but was not used in Tableau Dashboard
+
+---average ride length
+
+SELECT usertype AS member_casual,
+       AVG(DATEDIFF(MINUTE, CAST(starttime AS DATETIME), CAST(stoptime AS DATETIME))) AS avg_ride_duration_minutes
+FROM dbo.['Divvy_Trips_2015-Q1$']
+GROUP BY usertype;
+
+--max and min ride duration by member type
+
+SELECT usertype AS member_casual,
+       MAX(DATEDIFF(MINUTE, CAST(starttime AS DATETIME), CAST(stoptime AS DATETIME))) AS max_ride_duration_minutes,
+       MIN(DATEDIFF(MINUTE, CAST(starttime AS DATETIME), CAST(stoptime AS DATETIME))) AS min_ride_duration_minutes
+FROM dbo.['Divvy_Trips_2015-Q1$']
+GROUP BY usertype;
+
+
+
+
+
+
+
+
+
